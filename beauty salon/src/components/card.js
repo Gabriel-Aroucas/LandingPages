e48e392032f6addbb80e5052f3a6c8ab78d98b91
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-04-21 11:16:32 -0300

    update

---
+++ beauty salon/src/components/card.js
@@ -0,0 +1,10 @@
+/**
+ * model Card 
+ * 
+ * <div class="card">
+        <img src="https://via.placeholder.com/150" alt="waiting.." class="cardContent">
+        <div class="cardOverlay">
+            <h4></h4>
+        </div>
+    </div>
+ */
---
+++ beauty salon/src/components/modal.js
@@ -0,0 +1,19 @@
+/**
+ * Model Modal
+ * 
+<div class="modal">
+    <div class="src">
+        <img src="https://via.placeholder.com/150" alt="waiting">
+    </div>
+    <div class="title">
+        <h1></h1>
+    </div>
+    <div class="description">
+        <p></p>
+    </div>
+    <div class="button">
+        <button type="button" class="btnModal"></button>
+    </div>
+</div>
+ * 
+ */
