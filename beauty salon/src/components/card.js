effce7ec7bcb40914f0ee0ae509dd100525ce707
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-04-22 04:11:39 -0300

    update

--- beauty salon/src/components/card.js
+++ beauty salon/src/components/card.js
@@ -8,7 +8,15 @@
         </div>
     </div>
  */
-const imgSrc = 'https://via.placeholder.com/150';
-$(".cards").append("<div class='card'/>");
-$(".card").append(`<img src='${imgSrc}' alt='waiting'/>`);
-$(".card").append("<div class ='cardOverlay'/>");
+
+
+for (let i=0; i<3; i++){
+    const imgSrc = ['/images/bird.jpg'];
+    const h4 = ['Birds','Planes','Houses','Cars','Nature','Ocean','Bycicle']
+    
+    $(".cards").append(`<div class='card${i}'/>`);
+    $(`.card${i}`).append(`<img src='${imgSrc}' alt='waiting'/>`);
+    $(`.card${i}`).append(`<div class ='cardOverlay${i}'/>`);
+    $(`.cardOverlay${i}`).append(`<h4>${h4[i]}</h4>`);
+    
+}
