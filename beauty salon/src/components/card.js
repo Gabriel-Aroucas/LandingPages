4b8c1a71c96fa511a99f1506ebd2c01cf70c683d
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-05-09 17:18:54 -0300

    update

--- beauty salon/src/components/card.js
+++ beauty salon/src/components/card.js
@@ -1,22 +1,19 @@
-/**
- * model Card 
- * 
- * <div class="card">
-        <img src="https://via.placeholder.com/150" alt="waiting.." class="cardContent">
-        <div class="cardOverlay">
-            <h4></h4>
-        </div>
-    </div>
- */
+const bird = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/640px-Eopsaltria_australis_-_Mogo_Campground.jpg";
+const planes="https://s.yimg.com/ny/api/res/1.2/I3j1nOsma.bu0G53eLqu.Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02NTg-/https://s.yimg.com/os/creatr-uploaded-images/2021-12/b5c89610-56af-11ec-bca4-ee390166ceb2";
+const houses="https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg";
+const cars="https://www.volvocars.com/images/v/-/media/project/contentplatform/data/media/my23/car-images/c40-bev-my23.jpg?iar=0";
+const nature="https://thumbs.dreamstime.com/b/mola-e-natureza-184361891.jpg";
+const Ocean="https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1652341068/EducationHub/photos/ocean-waves.jpg";
+const Bycicle = "https://upload.wikimedia.org/wikipedia/commons/4/41/Left_side_of_Flying_Pigeon.jpg"
 
-
-for (let i=0; i<3; i++){
-    const imgSrc = ['/images/bird.jpg'];
+/*
+for (let i=0; i<7; i++){
+    const imgSrc = [bird,planes,houses,cars,nature,Ocean,Bycicle];
     const h4 = ['Birds','Planes','Houses','Cars','Nature','Ocean','Bycicle']
     
     $(".cards").append(`<div class='card${i}'/>`);
-    $(`.card${i}`).append(`<img src='${imgSrc}' alt='waiting'/>`);
+    $(`.card${i}`).append(`<img src='${imgSrc[i]}' alt='waiting'/>`);
     $(`.card${i}`).append(`<div class ='cardOverlay${i}'/>`);
     $(`.cardOverlay${i}`).append(`<h4>${h4[i]}</h4>`);
     
-}
+}*/
